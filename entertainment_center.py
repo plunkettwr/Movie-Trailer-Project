a178f212f2724f7f335c9fe3b641440c08794e4a
Robbie Plunkett <plunkettwr@gmail.com>
2017-11-30 13:21:03 -0500

    Ready for submission

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,8 +1,7 @@
 import media
 import fresh_tomatoes
 
-
-
+# movie objects to be added to the list below
 the_hitmans_bodyguard = media.Movie(
     "https://www.youtube.com/watch?v=F4Afusxc2SM",
     "tt1959563")
@@ -15,6 +14,7 @@ guardians_of_the_galaxy = media.Movie(
     "https://www.youtube.com/watch?v=d96cjJhvlMA",
     "tt2015381")
 
+# list to store movies
 movies = [the_hitmans_bodyguard,american_made,guardians_of_the_galaxy]
 
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -2,13 +2,16 @@ import urllib2
 import json
 
 class Movie():
+	"""Class to retrieve and store movie data."""
 
-    def __init__(self, trailer_youtube_url, imdb_id):
-        self.url = "http://www.omdbapi.com/?apikey=2299bccb&i="+imdb_id
-        self.data = json.load(urllib2.urlopen(self.url))
-        self.title = self.data['Title']
-        self.poster_image_url = self.data['Poster']
-        self.trailer_youtube_url = trailer_youtube_url
-        self.runtime = self.data['Runtime']
-        self.rated=self.data['Rated']
-        self.imdb_rating=self.data['imdbRating']
+	apikey = "2299bccb"  #enter your OMDb API key as a string
+
+	def __init__(self, trailer_youtube_url, imdb_id):    	
+                self.url = "http://www.omdbapi.com/?apikey="+self.apikey+"&i="+imdb_id
+                self.data = json.load(urllib2.urlopen(self.url))
+                self.title = self.data['Title']
+                self.poster_image_url = self.data['Poster']
+                self.trailer_youtube_url = trailer_youtube_url
+                self.runtime = self.data['Runtime']
+                self.rated=self.data['Rated']
+                self.imdb_rating=self.data['imdbRating']
